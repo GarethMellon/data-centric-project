c78e66881bf58b923cf8a29fd128aec94db75d19
Gareth_Mellon <garethmellon@gmail.com>
2019-02-05 11:30:33 +0000

    Added basic landing page displaying data

--- app.py
+++ app.py
@@ -26,11 +26,11 @@ def select_data(sql):
 
 @app.route('/')
 def index():
-    sql="select count(course.name), course.name as course_name , recipes.ID as recipes_ID, recipes.name as recipes_name from recipes JOIN course on recipes.ID = course.recipes_ID group by course.name;"
+    sql="select count(course.name) as count_course, course.name as course_name , recipes.ID as recipes_ID, recipes.name as recipes_name from recipes JOIN course on recipes.ID = course.recipes_ID group by course.name;"
     data = select_data(sql)
     
     return render_template("index.html",
-                            page_title="Home Page", 
+                            page_title="Welcome to you're cooking recieps", 
                             data = data)
 
     

b802adbf11db2cf1c3d073bac6a57caaa58bf819
Gareth_Mellon <garethmellon@gmail.com>
2019-02-05 17:31:53 +0000

    Add feature: index.html to display recieps in collapsable groups

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ app = Flask(__name__)
 
 username = os.getenv("C9_USER")
 
-connection = pymysql.connect(host = "localhost",
-                            user = username,
-                            password = "",
-                            db = "recipes")
-
 """
 Function to select data from mysql.  
 Pass in sql as string and will result the data as a list
 """
 def select_data(sql):
+    connection = pymysql.connect(host = "localhost",
+                            user = username,
+                            password = "",
+                            db = "recipes")
     try:
         with connection.cursor(pymysql.cursors.DictCursor) as cursor:
             cursor.execute(sql)
@@ -26,12 +25,17 @@ def select_data(sql):
 
 @app.route('/')
 def index():
-    sql="select count(course.name) as count_course, course.name as course_name , recipes.ID as recipes_ID, recipes.name as recipes_name from recipes JOIN course on recipes.ID = course.recipes_ID group by course.name;"
-    data = select_data(sql)
+    
+    recieps_sql="select course.name as course_name , recipes.ID as recipes_ID, recipes.name as recipes_name from recipes JOIN course on recipes.ID = course.recipes_ID;"
+    recipes_data = select_data(recieps_sql)
+    
+    course_sql ="select count(ID), name as course_name from course group by course_name;"
+    course_data = select_data(course_sql)
     
     return render_template("index.html",
-                            page_title="Welcome to you're cooking recieps", 
-                            data = data)
+                            page_title="Welcome to you're cooking recieps",
+                            course_data = course_data,
+                            recipes_data = recipes_data)
 
     
 if __name__ == '__main__':
--- static/planning/database/populate_database.sql
+++ static/planning/database/populate_database.sql
@@ -154,4 +154,89 @@ VALUES (1,"Contains Eggs");
 INSERT INTO course (recipes_ID, name)
 VALUES (1,"Breakfast");
 
+INSERT INTO recipes (name, youtube_link)
+VALUES ("Fluffy French Toast","https://www.youtube.com/watch?v=58_5h4Kym4Y");
 
+INSERT INTO directions (recipes_id, name)
+VALUES (2,"Measure flour into a large mixing bowl. Slowly whisk in the milk. Whisk in the salt, eggs, cinnamon, vanilla extract and sugar until smooth.");
+
+INSERT INTO directions (recipes_id, name)
+VALUES (2,"Heat a lightly oiled griddle or frying pan over medium heat.");
+
+INSERT INTO directions (recipes_id, name)
+VALUES (2,"Soak bread slices in mixture until saturated. Cook bread on each side until golden brown. Serve hot.");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (2,"All-purpose flour","1/4","Cup");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (2,"Milk","1","Cup");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (2,"Salt","1","Pinch");
+
+INSERT INTO ingredients (recipes_id, name,quantity)
+VALUES (2,"Eggs","3",);
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (2,"Ground Cinnamon","1/2","Teaspoon");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (2,"While Surag","1","Tablespoon");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (2,"Beard","12","Slices");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (2,"Salt","1","Pinch");
+
+INSERT INTO allergens (recipes_ID, name)
+VALUES (2,"Oats");
+
+INSERT INTO course (recipes_ID, name)
+VALUES (2,"Breakfast");
+
+INSERT INTO recipes (name, youtube_link)
+VALUES ("Cream Of Broccoli Soup","https://www.youtube.com/watch?v=_VzN2wG5XS0");
+
+INSERT INTO course (recipes_ID, name)
+VALUES (3,"Lunch");
+
+INSERT INTO directions (recipes_id, name)
+VALUES (3,"Melt 2 tablespoons butter in medium sized stock pot, and saute onion and celery until tender. Add broccoli and broth, cover and simmer for 10 minutes.");
+
+INSERT INTO directions (recipes_id, name)
+VALUES (3,"Pour the soup into a blender, filling the pitcher no more than halfway full. Hold down the lid of the blender with a folded kitchen towel, and carefully start the blender, using a few quick pulses to get the soup moving before leaving it on to puree. Puree in batches until smooth and pour into a clean pot. Alternately, you can use a stick blender and puree the soup right in the cooking pot.");
+
+INSERT INTO directions (recipes_id, name)
+VALUES (3,"In small saucepan, over medium-heat melt 3 tablespoons butter, stir in flour and add milk. Stir until thick and bubbly, and add to soup. Season with pepper and serve.");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (3,"Butter","2","Tablespoons");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (3,"Onion","1","Chopped");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (3,"Celery Stalk","1","Chopped");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (3,"Chicken Broth","3","Cups");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (3,"Broccoli Florets","8","Cups");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (3,"Butter","3","Tablespoons");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (3,"All-Purpose Flour","3","Tablespoons");
+
+INSERT INTO ingredients (recipes_id, name,quantity,unit_of_measurement)
+VALUES (3,"Mile","2","Cups");
+
+INSERT INTO ingredients (recipes_id, name)
+VALUES (3,"Ground Black Pepper");
+
+INSERT INTO course (recipes_ID, name)
+VALUES (3,"Lunch");
